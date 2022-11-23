a1a3f194bcedd1deee7e11acc17a0fe9bbd6c0a7
Reldean-Williams <rewillia@tcd.ie>
2022-11-23 09:27:04 +0000

    My MedianFilter

--- MedianFilter.py
+++ MedianFilter.py
@@ -1,10 +1,18 @@
-from sympy import array
 import numpy as np
 
 def median_filter(data, filter_size):
-    data = filter_size / 2
-    padding = np.zeros((len(data),len(data[0])))
-    for i in range(len(data)):
+    data = [1, 2, 3, 6, 10, 7, 1]                       
+    N = len(data)
+    filter_size = sorted(data)
+    indexer = (filter_size - 1) // 2
+    med_filter = (data[0])
+    padding = np.zeros((N, len(data[0])))
+    for i in range(0, N - 2):
+       print(data[i], data[i + 1], data[i + 2])
+       x = median_filter(data[i], data[i + 1], data[i + 2])
+    med_filter.append(x)
+    med_filter.append(data(-1))
+    print(med_filter)
 
-     #arr = []
-#removed_noise = median_filter(arr, 5)
+
+
